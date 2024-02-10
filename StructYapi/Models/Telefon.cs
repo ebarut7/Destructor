e5139d9bf1cede81b6414bc29261ffc2f7aeb470
Hakan şahin <hknshn55@hotmail.com>
2024-02-10 19:42:34 +0300

    Struct Yapi

---
+++ StructYapi/Models/Telefon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructYapi.Models
+{
+    internal struct Telefon
+    {
+        public decimal Fiyati;
+        public string Model { get; set; }
+        public string Marka;
+        public int Hafiza;
+    }
+}
---
+++ StructYapi/Program.cs
@@ -0,0 +1,24 @@
+﻿using StructYapi.Models;
+
+namespace StructYapi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Telefon telefon = new Telefon();
+            telefon.Marka = "Samsun 55";
+            telefon.Hafiza = 200;
+            telefon.Fiyati = 999;
+            telefon.Model = "55";
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+
+        }
+}
+}
