6b88e1e25ad6440a30945fb7591a12d16110c1d6
Hakan şahin <hknshn55@hotmail.com>
2024-02-10 16:58:00 +0300

    Destructor ornegi yapildi.

---
+++ Destructor/Models/Araba.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Destructor.Models
+{
+    public class Araba
+    {
+        public Araba()
+        {
+            Console.WriteLine("Araba olusturuldu.");
+        }
+
+
+        ~Araba()
+        {
+            Console.WriteLine("İş bitti");
+        }
+
+    }
+}
--- Destructor/Program.cs
+++ Destructor/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Destructor
+﻿using Destructor.Models;
+
+namespace Destructor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Constructor : Yapici metot anlamina gelmektedir. Sinif insa edilirken yani olusturlurken calisan metotdur. 
+
+            for (int i = 0; i < 100; i++)
+            {
+                new Araba();
+                GC.Collect();
+            }
+           
+            // Destructor : Yikici metot sinifin isi bittiginde calisacak metodudur. Gc temizleyecegi anda artik nesnenin isis bitmis demektir ve temzilenmeden once destructor sayesinde son islemlerini gerceklestirir.
+
+
         }
     }
 }
